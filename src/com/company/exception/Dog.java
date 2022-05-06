567b8bd16050dc22c4e46d15f85948615e98d0b2
ZhukovAlV <Olenka2012$github>
2022-05-06 15:50:35 +0600

    Добавлен пример Exception

---
+++ src/com/company/exception/Dog.java
@@ -0,0 +1,48 @@
+package com.company.exception;
+
+public class Dog {
+
+    String name;
+    boolean isCollarPutOn;
+    boolean isLeashPutOn;
+    boolean isMuzzlePutOn;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) throws DogIsNotReadyException {
+        Dog dog = new Dog("Мухтар");
+        dog.putCollar();
+        dog.putMuzzle();
+        dog.walk();//Unhandled exception: DogIsNotReadyException
+    }
+
+    public void putCollar() {
+
+        System.out.println("Ошейник надет!");
+        this.isCollarPutOn = true;
+    }
+
+    public void putLeash() {
+
+        System.out.println("Поводок надет!");
+        this.isLeashPutOn = true;
+    }
+
+    public void putMuzzle() {
+        System.out.println("Намордник надет!");
+        this.isMuzzlePutOn = true;
+    }
+
+    public void walk() throws DogIsNotReadyException {
+
+        System.out.println("Собираемся на прогулку!");
+        if (isCollarPutOn && isLeashPutOn && isMuzzlePutOn) {
+            System.out.println("Ура, идем гулять! " + name + " очень рад!");
+        } else {
+            throw new DogIsNotReadyException("Собака " + name + " не готова к прогулке! Проверьте экипировку!");
+        }
+    }
+
+}
---
+++ src/com/company/exception/DogIsNotReadyException.java
@@ -0,0 +1,8 @@
+package com.company.exception;
+
+public class DogIsNotReadyException extends Exception {
+
+    public DogIsNotReadyException(String message) {
+        super(message);
+    }
+}
